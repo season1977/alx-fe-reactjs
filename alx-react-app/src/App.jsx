d350bc8e0e36800d420fb86e84f2abb36b054043
unknown <abdulrasaqbellow@gmail.com>
2025-08-31 21:16:15 +0100

    Fix: add WelcomeMessage import and component usage in App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +17,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
+      <Highlight, I'm Abdulrasaq Bello Welcome to my first react App />
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}

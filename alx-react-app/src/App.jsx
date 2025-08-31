e5c88e439b1b7f4e30d77a63a63b3ee5d88abcea
unknown <abdulrasaqbellow@gmail.com>
2025-08-31 21:20:50 +0100

    Fix: add WelcomeMessage import and component usage in App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
+import I'm Abdulrasaq Bello Welcome to my first react App />
+ from './components/WelcomeMessage'
 
 function App() {
   const [count, setCount] = useState(0)

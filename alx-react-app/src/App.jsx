98ae80d9de0a69bb228e4f5964e0c132e75ec11d
unknown <abdulrasaqbellow@gmail.com>
2025-08-31 22:11:59 +0100

    Add UserProfile component and use props in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,15 +2,17 @@ import './App.css'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 function App() {
   return (
     <>
       <Header />
       <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <Footer />
     </>
   )
 }
 
-export default App;
+export default App
